4a8040f05908378ef94a38ac395084f5b435bf68
Nawintesting <testingnawin@gmail.com>
2023-04-26 16:26:31 +0530

    GIT CHeck commit 3 rd commit

---
+++ src/com/git/testcases/GitTestCase.java
@@ -0,0 +1,13 @@
+package com.git.testcases;
+
+import java.sql.Date;
+
+public class GitTestCase {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Git TestCase"+System.currentTimeMillis());		
+		
+	}
+
+}
