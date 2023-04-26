18d15d118a61eb2c1ac419eea1ed0580f82ce8ad
Nawintesting <testingnawin@gmail.com>
2023-04-26 16:51:49 +0530

    Adding hello() method

--- src/com/git/testcases/GitRecheck.java
+++ src/com/git/testcases/GitRecheck.java
@@ -1,12 +1,19 @@
 package com.git.testcases;
-
+class GitCheck{
+	
+	public void hello(){
+		System.out.println("Hello method");
+	}
+	
+}
 public class GitRecheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		System.out.println("GIT RECHECK");
 		System.out.println("MOdified 1");
+	
 	}
-
 }
